2ee6f7cf0ae926a9763511425be6928fb95fdd25
kimimahri <kimimahri@gmail.com>
2020-11-29 13:29:20 +0300

    registration with short password test

---
+++ src/test/java/RegistrationTest.java
@@ -0,0 +1,41 @@
+import constants.enums.Notices;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import steps.UserSteps;
+
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(SerenityRunner.class)
+public class RegistrationTest {
+
+    @Managed
+    WebDriver driver;
+    private List<String> bad_email = Collections.singletonList(Notices.INCORRECT_EMAIL.getTitle());
+    private List<String> short_password = Collections.singletonList(Notices.SHORT_PASSWORD.getTitle());
+    @Steps
+    private UserSteps user;
+
+    @Test
+    @Title("Регистрация с некорректным email")
+    public void registrationWithIncorrectEmailTest() {
+        user.open_registration_page()
+                .enter_email_and_password("badEmail", "password")
+                .click_registration_button()
+                .should_see_notices(bad_email);
+    }
+
+    @Test
+    @Title("Регистрация с паролем менее 8 символов")
+    public void registrationWithShortPasswordTest() {
+        user.open_registration_page()
+                .enter_email_and_password("goodtestemail@testemail.com", "pswrd")
+                .click_registration_button()
+                .should_see_notices(short_password);
+    }
+}
